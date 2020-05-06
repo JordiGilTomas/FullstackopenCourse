897f32b4f6b116f50eed74fac589934697920e7f
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-05-06 16:39:29 +0200

    2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, part) => acc + part.exercises, 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+export default Course;
