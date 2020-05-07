b68d8c33efbf5ad5c5734b9c93631b91218b9712
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-05-07 16:22:09 +0200

    2.11

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PersonForm from "./PersonForm";
 import Filter from "./Filter";
 import Persons from "./Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNumber] = useState("");
   const [newFilter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const isNotNew = () => {
     return [...persons].find((person) => person.name === newName);
